086aab6b85b231ed5db36e761e606ea866864e37
drighetto <drighetto@excellium-services.com>
2021-12-18 20:28:10 +0100

    Initial add

--- packaging.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mvn clean package
-mvn dependency:copy-dependencies
-mkdir -p target/dist/lib
-cp target/dependency/*.jar target/dist/lib/
-cp target/*.jar  target/dist/
-cp run.sh target/dist/
